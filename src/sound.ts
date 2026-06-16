1b160b2582c9e342b366825553f6f0d26d4c41d3
js0ny <contact@js0ny.net>
2026-06-16 06:58:15 +0100

    fix: ensure mpv process exits when stopping playback

--- src/sound.ts
+++ src/sound.ts
@@ -94,7 +94,18 @@ async function playOnLinux(soundPath: string, volume: number): Promise<void> {
   const players = [
     { command: "paplay", args: [`--volume=${pulseVolume}`, soundPath] },
     { command: "aplay", args: [soundPath] },
-    { command: "mpv", args: ["--no-video", "--no-terminal", "--script-opts=autoload-disabled=yes", `--volume=${percentVolume}`, soundPath] },
+    {
+      command: "mpv",
+      args: [
+        "--no-video",
+        "--no-terminal",
+        "--script-opts=autoload-disabled=yes",
+        "--keep-open=no",
+        "--idle=no",
+        `--volume=${percentVolume}`,
+        soundPath,
+      ],
+    },
     { command: "ffplay", args: ["-nodisp", "-autoexit", "-loglevel", "quiet", "-volume", `${percentVolume}`, soundPath] },
   ]
 
